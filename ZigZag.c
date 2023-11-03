5dc3c16cf20a2cb0fabf9ff09bb2d1f84f6cda42
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-11-03 21:08:37 +0530

    Add files via upload

---
+++ ZigZag.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int r ,c;
+    for(r=0;r<3;r++)
+    {
+        for(c=0;c<9;c++)
+        {
+            if((r+c)%4==0 || r==2 && c%4==0)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
