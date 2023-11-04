85cb7a0c87683123ac969d6b9eb934b8bb6273d0
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-11-04 07:02:04 +0530

    Add files via upload

---
+++ HalfTriangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c;
+    for(r=1;r<5;r++)
+    {
+        for(c=1;c<5;c++)
+        {
+            printf(" * ");
+            c += c;
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
