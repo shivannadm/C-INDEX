9ccf3b072a52a6604feb1bbe87b9a6e2197b7543
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-11-04 07:02:25 +0530

    Add files via upload

---
+++ Square.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c;
+    for(r=1;r<7;r++)
+    {
+        for(c=1;c<7;c++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
