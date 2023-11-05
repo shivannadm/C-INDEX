068d3e53e90344022bc3e5b82b8529c3e2e3c3ad
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-11-05 18:30:24 +0530

    Add files via upload

---
+++ Triangle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+
+    // Straight traingle
+
+    for (r = 1; r < 5; r++)
+    {
+        for (c = 1; c < 8; c++)
+        {
+            if (r == 4 && c < 8 || c - r == 3 || r + c == 5 || r != 1 && r + c == 6 || r == 3 && c == 4 || r == 3 && c == 5)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // Inverse Triangle
+    for (r = 1; r < 5; r++)
+    {
+        for (c = 1; c < 8; c++)
+        {
+            if (r == 1 && c < 8 || r - c == 0 || r + c == 8 || (r == 2 || r == 3) && r + c == 7 || r == 2 && r + c == 6 || r == 2 && r + c == 5)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
